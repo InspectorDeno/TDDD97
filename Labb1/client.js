757ce4bb0fb8ef593c02f795d0a00913b2fedcbf
InspectorDeno <dennisdufback@gmail.com>
2018-01-23 10:49:41 +0100

    load views

---
+++ Labb1/client.js
@@ -0,0 +1,18 @@
+
+displayView = function(){
+
+    document.getElementById("view").innerHTML = document.getElementById("welcomeview").innerHTML;
+};
+
+
+window.onload = function(){
+        document.getElementById("view").innerHTML = document.getElementById("welcomeview").innerHTML;
+
+};
+
+login = function(){
+
+};
+signup = function(){
+
+};
