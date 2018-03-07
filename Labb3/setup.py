d2f51ed779f16944e98e75e7b1a0310c45e4ca94
gusva680 <Qxe326>
2018-03-07 16:51:12 +0100

    add seperate folder for labb 3

---
+++ Labb3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='Twidder',
+    version='0.2',
+    packages=['Twidder'],
+    url='',
+    license='',
+    author='dennisdufback',
+    author_email='',
+    description='',
+    include_package_data=True,
+    install_requires=[
+        'flask',
+        'gevent',
+        'gevent-websocket',
+    ]
+)
