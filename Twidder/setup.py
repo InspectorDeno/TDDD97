9d69c327207fc6f9fcca0337077d3b4676c3e2b5
InspectorDeno <dennisdufback@gmail.com>
2018-03-07 15:56:20 +0100

    add setup

---
+++ Twidder/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='Twidder',
+    version='0.2',
+    packages=['Twidder'],
+    url='',
+    license='',
+    author='dennisdufback',
+    author_email='',
+    description='',
+    include_package_data=True,
+    install_requires=[
+        'flask',
+        'gevent',
+    ]
+)
